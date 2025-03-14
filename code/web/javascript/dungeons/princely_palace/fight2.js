498f603f7c806b1e55ea4be70d7ca710dc24fd94
40444015 <mwallace19@qub.ac.uk>
2025-03-14 15:15:45 +0000

    Dungeon5 palace

---
+++ code/web/javascript/dungeons/princely_palace/fight2.js
@@ -0,0 +1,7 @@
+function jump(){
+    window.location.href = "victory.html";
+}
+
+function duck(){
+    window.location.href = "gold.html";
+}
---
+++ code/web/settings.js
@@ -0,0 +1,6 @@
+//This obj will be fetched from sql and changed in the settings page
+
+let settings = {
+    doTextAnimations: true,
+    textAnimationSpeed: 30, //controlled by slider with values 20 - 70
+}

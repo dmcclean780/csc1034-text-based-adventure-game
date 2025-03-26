70ce4128c1d152e1f0e40438c0510bd626c01984
40444015 <mwallace19@qub.ac.uk>
2025-03-26 10:34:09 +0000

    Palace db FINISHED YIPPEE

--- code/web/authDetails.js
+++ code/web/authDetails.js
@@ -1,7 +1,7 @@
 authDetails = {
-    "webhosting_url": "jduke02.webhosting1.eeecs.qub.ac.uk",
+    "webhosting_url": "mwallace19.webhosting1.eeecs.qub.ac.uk",
     "hostname": "localhost",
-    "username": "jduke02",
-    "password": "vnrsMq16fMSXHNB8",
-    "database": "jduke02"
+    "username": "mwallace19",
+    "password": "3kDX3ylp60JBQySS",
+    "database": "mwallace19"
 }

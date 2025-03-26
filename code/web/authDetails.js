9a2f8528037f7ecdf09e141d596fd3a1b1bda959
Matthew Wallace <mwallace19@qub.ac.uk>
2025-03-26 01:12:10 +0000

    palaceDB4

--- code/web/authDetails.js
+++ code/web/authDetails.js
@@ -1,7 +1,7 @@
 authDetails = {
-    "webhosting_url": "",
-    "hostname": "",
-    "username": "",
-    "password": "",
-    "database": ""
+    "webhosting_url": "mwallace19.webhosting1.eeecs.qub.ac.uk",
+    "hostname": "localhost",
+    "username": "mwallace19",
+    "password": "3kDX3ylp60JBQySS",
+    "database": "mwallace19"
 }
